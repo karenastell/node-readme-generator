f6bb46c7bad42b8872eb2c9ade33860d8f976b94
Karen Astell <KarenAstell@gmail.com>
2020-05-29 11:17:04 -0400

    started on generateMarkdown function

--- Develop/index.js
+++ Develop/index.js
@@ -7,111 +7,97 @@ const fs = require("fs");
 // includes the generateMarkdown function from generateMarkdown.js
 const generateMarkdown = require("./utils/generateMarkdown");
 
+// array of questions
+const questions = [
+  {
+    //badge
+    type: "input",
+    name: "badge",
+    message: "Enter a BADGE:",
+  },
+  {
+    // * Project title
+    type: "input",
+    name: "title",
+    message: "Enter your PROJECT TITLE:",
+  },
+  {
+    // * Description
+    type: "input",
+    name: "description",
+    message: "Enter a DESCRIPTION of your project:",
+  },
+  // {
+  //   // * Table of Contents:
+  //   type: "checkbox",
+  //   name: "contents",
+  //   message: "Choose your TABLE OF CONTENTS:",
+  //   choices: [1, 2, 3, 4]
+  // },
+  {
+    // * Installation: string
+    type: "input",
+    name: "install",
+    message: "Enter INSTALLATION instructions:",
+  },
+  {
+    // * Usage: string
+    type: "input",
+    name: "usage",
+    message: "Enter USAGE instructions:",
+  },
+  {
+    // * License: string
+    type: "list",
+    name: "license",
+    message: "Enter LICENSE information:",
+    choices: ["BSD", "MIT", "GPL","Other", "No License"],
+  },
+  {
+    // * Contributing: array
+    type: "input",
+    name: "contributors",
+    message: "Enter any CONTRIBUTORS to your project:",
+  },
+  {
+    // * Tests: string
+    type: "input",
+    name: "test",
+    message: "Enter TESTS done on your project:",
+  },
+  {
+    //   * User GitHub profile picture
+    type: "input",
+    name: "githubPic",
+    message: "Upload your GITHUB PROFILE PICTURE:",
+  },
+  {
+    // GitHub email
+    type: "input",
+    name: "email",
+    message: "Enter your GITHUB EMAIL:",
+  },
+];
 
-  // array of questions
-  const questions =[
-    {
-      //badge
-      type: "input",
-      name: "badge",
-      message: "Enter a BADGE:"
+// where to save the file
+const writeFile = (fileName, data)=>{
+  fs.writeFile(fileName, generateMarkdown(data), (err)=> {
+  if (err) {
+    throw err;
+  }
+  console.log("file saved to README");
+})
+}
 
-    },
-    {
-      // * Project title
-      type: "input",
-      name: "title",
-      message: "Enter your PROJECT TITLE:",
-      
-    },
-    {
-      // * Description
-      type: "input",
-      name: "description",
-      message: "Enter a DESCRIPTION of your project:",
-      
-    },
-    {
-      // * Table of Contents:
-      type: "input",
-      name: "contents",
-      message: "Enter your TABLE OF CONTENTS:",
-      
-    },
-    {
-      // * Installation: string
-      type: "input",
-      name: "install",
-      message: "Enter INSTALLATION instructions:",
-      
-    },
-    {
-      // * Usage: string
-      type: "input",
-      name: "usage",
-      message: "Enter USAGE instructions:",
-      
-    },
-    {
-      // * License: string
-      type: "list",
-      name: "license",
-      message: "Enter LICENSE information:",
-      choices: ["BSD", "MIT", "GPL", "No License"]
-      
-    },
-    {
-      // * Contributing: array
-      type: "input",
-      name: "contributors",
-      message: "Enter any CONTRIBUTORS to your project:",
-      
-    },
-    {
-      // * Tests: string
-      type: "input",
-      name: "test",
-      message: "Enter TESTS done on your project:",
-      
-    },
-    {
-      // * Questions: array
-      //   * User GitHub profile picture
-      //   * User GitHub email
-      type: "input",
-      name: "github",
-      message: "Upload your GITHUB PROFILE PICTURE and enter your GITHUB EMAIL:",
-     
-    }
-  ]
-//   .then((answers)=>{
-//   console.log("TITLE", answers.title);
-// })
-
-// fs.writeFile("person.html", html, (error) => {
-//     if (error) {
-//         console.error(error);
-//     }
-
-//     console.log("File saved successfully!");
-// })
-
-  // where to save the file
-  // fs.writeFile("README.md", questions, (err)=> {
-  //   if (err) {
-  //     throw err;
-  //   }
-  //   console.log("file saved to README");
-  // })
-  // create a file with name fileName
-  // writeto file fileName the data
-
+// create a file with name fileName
+// writeto file fileName the data
 
 function init() {
   // initialize the prompting of questions
-  inquirer.prompt(questions).then((answers)=>{
+  inquirer.prompt(questions).then((answers) => {
     console.log(answers);
-  })
+  });
+  writeFile("README.md", answers);
 }
 
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,16 @@
 function generateMarkdown(data) {
   return `
 # ${data.title}
+# ${data.badge}
+# ${data.title}
+# ${data.description}
+# ${data.install}
+# ${data.usage}
+# ${data.license}
+# ${data.contributors}
+# ${data.test}
+# ${data.githubPic}
+# ${data.email}
 
 `;
 }
